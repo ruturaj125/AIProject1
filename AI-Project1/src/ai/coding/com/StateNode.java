245248dda2853eec0d528b1be6cf9aab0dfb3650
Ruturaj Patil <ruturajpatil97@gmail.com>
2022-05-13 10:24:23 -0700

    StateNode class added

--- AI-Project1/src/ai/coding/com/StateNode.java
+++ AI-Project1/src/ai/coding/com/StateNode.java
@@ -1,5 +1,191 @@
 package ai.coding.com;
 
+import java.util.*;
+
 public class StateNode {
+
+	public int function_value;
+	public int[][] positions;
+	public int gn = 0;
+	public static int input_type; 
+	
+	public StateNode(int[][] a, int level, int input)
+	{
+		StateNode.input_type = input;
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				this.positions[i][j] = a[i][j];
+			}
+		}
+		this.gn = level;
+		this.function_value = calculateFunctionValue();
+	}
+	
+	public int calculateFunctionValue()
+	{
+		int hn = 0;
+		if(StateNode.input_type == 1)
+		{
+			hn = 0;
+		}
+		if(StateNode.input_type == 2)
+		{
+			hn = getMisplacedTileHeuristic();
+		}
+		if(StateNode.input_type == 3)
+		{
+			hn = getManhattanDistance();
+		}
+		
+		return hn + this.gn;
+	}
+	
+	private int getMisplacedTileHeuristic()
+	{
+		int sum=0;
+		int N = Project.goal.length;
+		for (int i = 0;i<N;i++)
+		{
+			for (int j = 0; j<N; j++)
+			{
+				if(this.positions[i][j] == 0)
+				{
+					continue;
+				}
+				if(this.positions[i][j] != Project.goal[i][j])
+					sum+=1;
+			}
+		}
+		return sum;
+	}
+	
+	private int getManhattanDistance()
+	{
+		int ans=0;
+		int[] index= new int[2];
+		
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if(this.positions[i][j] == 0)
+				{
+					continue;
+				}
+				index = findPositionInGoal(this.positions[i][j]);
+				ans = ans + (Math.abs(i-index[0])+Math.abs(j-index[1]));
+			}
+		}
+		return ans;
+	}
+	
+	private int[] findPositionInGoal(int value)
+	{
+		int[] ans = new int[2];
+		
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if(Project.goal[i][j] == 0)
+				{
+					continue;
+				}
+				if (Project.goal[i][j] == value)
+				{
+					ans[0]=i;
+					ans[1]=j;
+					return ans;
+				}
+			}
+		}
+		
+		return ans;
+	}
+	
+	public List<StateNode> getChildrens()
+	{
+		ArrayList<StateNode> childrens = new ArrayList<StateNode>();
+		
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				if(this.positions[i][j] == 0)
+				{
+					if(i-1>=0)
+					{
+						int[][] child = new int[3][3];
+						for (int x = 0; x < 3; x++)
+						{
+							for(int y = 0; y < 3; y++)
+							{
+								child[x][y] = this.positions[x][y];
+							}
+						}
+						 child = swap(child,i,j,i-1,j);
+						 StateNode b = new StateNode(child,this.gn+1, StateNode.input_type);
+						 childrens.add(b);
+					}
+					if(j-1>=0)
+					{
+						int[][] child = new int[3][3];
+						for (int x = 0; x < 3; x++)
+						{
+							for(int y = 0; y < 3; y++)
+							{
+								child[x][y] = this.positions[x][y];
+							}
+						}
+						 child = swap(child,i,j,i,j-1);
+						 StateNode b = new StateNode(child,this.gn+1, StateNode.input_type);
+						 childrens.add(b);
+					}
+					if(i+1>=0)
+					{
+						int[][] child = new int[3][3];
+						for (int x = 0; x < 3; x++)
+						{
+							for(int y = 0; y < 3; y++)
+							{
+								child[x][y] = this.positions[x][y];
+							}
+						}
+						 child = swap(child,i,j,i+1,j);
+						 StateNode b = new StateNode(child,this.gn+1, StateNode.input_type);
+						 childrens.add(b);
+					}
+					if(j+1>=0)
+					{
+						int[][] child = new int[3][3];
+						for (int x = 0; x < 3; x++)
+						{
+							for(int y = 0; y < 3; y++)
+							{
+								child[x][y] = this.positions[x][y];
+							}
+						}
+						 child = swap(child,i,j,i,j+1);
+						 StateNode b = new StateNode(child,this.gn+1, StateNode.input_type);
+						 childrens.add(b);
+					}
+				}
+			}
+		}
+		
+		return childrens;
+	}
+	
+	private int[][] swap(int[][] a,int row1, int col1, int row2, int col2)
+	{
+	     int[][] copy = a;
+	     int tmp = copy[row1][col1];
+	     copy[row1][col1] = copy[row2][col2];
+	     copy[row2][col2] = tmp;
+	
+	     return copy;
+	}
 
 }
