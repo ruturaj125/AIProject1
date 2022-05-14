b4a544408d6885c09b1b7ad129b065bb01db4fce
Ruturaj Patil <ruturajpatil97@gmail.com>
2022-05-14 12:36:43 -0700

    Comments added

--- AI-Project1/src/ai/coding/com/Project.java
+++ AI-Project1/src/ai/coding/com/Project.java
@@ -2,27 +2,43 @@ package ai.coding.com;
 
 import java.util.*;
 
+// Runner class where main function is present
+// This class holds generalSearch method
 public class Project {
 	
-	public static PriorityQueue<StateNode> queue = new PriorityQueue<StateNode>(); 
+	// Priority queue is used to compare and choose next state
+	public static PriorityQueue<StateNode> queue = new PriorityQueue<StateNode>();
+	// Set is used to handle duplication
 	public static Set<int[][]> chosenNodes = new HashSet<int[][]>();
+	// Path to the solution
 	public static ArrayList<StateNode> ans = new ArrayList<StateNode>();
+	
+	// Goal State
 	public static int[][] goal;
 	
+	// Rows and columns value, to change from 8-puzzle to 15-puzzle edit following values
+	public static int rows = 3, columns = 3;
+	
+	// General Search Algorithm as discussed in class
 	public static void generalSearch(StateNode node,int input){
         
+		// Initial State added in queue
 		queue.add(node);
         
         while(!queue.isEmpty()) {
             
+        	// Choose optimal path next state
         	StateNode current=queue.poll();
         	chosenNodes.add(current.positions);
             ans.add(current);
+            
+            // Check if chosen state is a goal state or not
         	if (Arrays.deepEquals(current.positions, goal)) {
                 System.out.println("Goal reached");
                 break;
             }
             
+        	// Add children states into queue
         	ArrayList<StateNode> list = current.getChildrens();
         	for(StateNode child: list)
         	{
@@ -32,38 +48,43 @@ public class Project {
         		}
         	}
         }
+        // Function values print
         for( StateNode node1 :ans) {
             resultPrint(node1);
             System.out.println("f(n) :" + node1.function_value);
             System.out.println("h(n) :" + (node1.function_value - node1.gn));
             System.out.println("g(n) :" + (node1.gn));
         }
-        System.out.println("Answer size "+ ans.size());
+        //System.out.println("Answer size "+ ans.size());
     }
 	
+	// Funtion to print satate values
 	public static void resultPrint( StateNode currentNode){
         
-        for (int l = 0; l < 3; l++) {
-            for (int m = 0; m < 3; m++) {
+        for (int l = 0; l < rows; l++) {
+            for (int m = 0; m < columns; m++) {
                 System.out.print(currentNode.positions[l][m] + "\t");
             }
             System.out.println();
         }
     }
 	
+	// Main function
 	public static void main(String args[])
 	{
-		int[][] initialState = new int[3][3];
-		System.out.println("Hi!! This is the 8-puzzle. Please enter the number to select the heuristic.");
-        System.out.println("Press 1: Uniform Cost Search");
-        System.out.println("Press 2: A* with Misplaced tile heuristic");
-        System.out.println("Press 3: A* with Manhattan distance heuristic");
+		// Decide which heuristic
+		int[][] initialState = new int[rows][columns];
+		System.out.println("Welcome..!! This is The 8-puzzle.\n Please enter the number to select the heuristic.");
+        System.out.println("Type 1: Uniform Cost Search");
+        System.out.println("Type 2: A* with Misplaced tile heuristic");
+        System.out.println("Type 3: A* with Manhattan distance heuristic");
         Scanner scanner = new Scanner(System.in);
         
         int input= scanner.nextInt();
         
-        System.out.println("Press 1: If you want to solve puzzle with default input.");
-        System.out.println("Press 2: If you want to solve puzzle with your input.");
+        // Decide to use default input or custom input
+        System.out.println("Type 1: If you want to solve puzzle with default input.");
+        System.out.println("Type 2: If you want to solve puzzle with your input.");
         
         int isDefault= scanner.nextInt();
         
@@ -78,9 +99,9 @@ public class Project {
         {
         	System.out.println("Enter initial state. **Valid input contains numbers 0 to 8. **0 treated as space.");
             
-            for(int i = 0; i< 3; i++)
+            for(int i = 0; i< rows; i++)
             {
-            	for(int j =0; j< 3; j++)
+            	for(int j =0; j< columns; j++)
             	{
             		int val = scanner.nextInt();
             		initialState[i][j] = val;
@@ -93,19 +114,25 @@ public class Project {
                 {7, 8, 0}};
 		Project.goal = goal;
 		long startTime=System.currentTimeMillis();
+		
+		// Create initial state with level 0
 		StateNode state = new StateNode(initialState,0, input);
+		
+		// Check if puzzle is solvable or not
 		boolean flag = isSolvable(initialState);
 		
 		if(flag)
 		{
+			// general search
 			generalSearch(state,input);
-			for( StateNode node1 :ans) {
+			//for( StateNode node1 :ans) {
 	            //resultPrint(node1);
 	            //System.out.println("f(n) :" + node1.function_value);
 	            //System.out.println("h(n) :" + (node1.function_value - node1.gn));
 	            //System.out.println("g(n) :" + (node1.gn));
-	        }
+	        //}
 	        System.out.println("Answer size "+ ans.size());
+	        System.out.println("Total Nodes generated:"+(ans.size()+queue.size()));
 		}
 		else
 		{
@@ -115,26 +142,32 @@ public class Project {
 		System.out.println("Time Taken in milli seconds: "+(endTime-startTime));
 	}
 	
+	// Function to check if puzzle is solvable or not
 	private static boolean isSolvable(int[][] array)
 	{
-		int[] seq = new int[9];
+		int[] seq = new int[rows*columns];
 		int index = 0;
-		for (int i = 0; i < 3; i++) 
+		for (int i = 0; i < rows; i++) 
 		{
-            for (int j = 0; j < 3; j++) 
+            for (int j = 0; j < columns; j++) 
             {
-                seq[index++] = array[i][j]; 
+            	if(array[i][j] != 0)
+            	{	
+	            	seq[index] = array[i][j]; 
+	            	index++;
+            	}
             }
         }
 		return (getInversions(seq) % 2) == 0;
 	}
 	
+	// Get number of inversions from 1-d array
 	private static int getInversions(int[] arr)
 	{
 	    int count = 0;
-	    for (int i = 0; i < 8; i++)
+	    for (int i = 0; i < rows*columns - 1; i++)
 	    {
-	    	for (int j = i+1; j < 9; j++)
+	    	for (int j = i+1; j < rows*columns; j++)
 	    	{
 	    		if(arr[j]>arr[i]){
 	    			count++;
--- AI-Project1/src/ai/coding/com/StateNode.java
+++ AI-Project1/src/ai/coding/com/StateNode.java
@@ -2,20 +2,23 @@ package ai.coding.com;
 
 import java.util.*;
 
+// This class holds information of state along with its functional value
 public class StateNode implements Comparable<StateNode> {
 
-	public int function_value;
-	public int[][] positions;
-	public int gn = 0;
+	public int function_value; // Function value to compare between states
+	public int[][] positions; // Current state of tiles
+	public int gn = 0; // g(n)
 	public static int input_type; 
+	public static int rows = Project.rows, columns = Project.columns;
 	
+	// Constructor will take tile positions, level and input heuristic
 	public StateNode(int[][] a, int level, int input)
 	{
 		StateNode.input_type = input;
-		this.positions = new int[3][3];
-		for(int i = 0; i < 3; i++)
+		this.positions = new int[rows][columns];
+		for(int i = 0; i < rows; i++)
 		{
-			for(int j = 0; j < 3; j++)
+			for(int j = 0; j < columns; j++)
 			{
 				this.positions[i][j] = a[i][j];
 			}
@@ -24,23 +27,25 @@ public class StateNode implements Comparable<StateNode> {
 		this.function_value = calculateFunctionValue();
 	}
 	
+	// Function to calculate f(n) = h(n) + g(n)
 	public int calculateFunctionValue()
 	{
 		return calculateHNValue() + this.gn;
 	}
 	
+	// Function gives h(n) value for selected heuristic
 	public int calculateHNValue()
 	{
 		int hn = 0;
-		if(StateNode.input_type == 1)
+		if(StateNode.input_type == 1) // Uniform cost search
 		{
 			hn = 0;
 		}
-		if(StateNode.input_type == 2)
+		if(StateNode.input_type == 2) // Misplaced tile
 		{
 			hn = getMisplacedTileHeuristic();
 		}
-		if(StateNode.input_type == 3)
+		if(StateNode.input_type == 3)// Manhattan Distance
 		{
 			hn = getManhattanDistance();
 		}
@@ -48,6 +53,7 @@ public class StateNode implements Comparable<StateNode> {
 		return hn;
 	}
 	
+	// H(n) value for Misplaced tile heuristic
 	private int getMisplacedTileHeuristic()
 	{
 		int sum=0;
@@ -67,14 +73,15 @@ public class StateNode implements Comparable<StateNode> {
 		return sum;
 	}
 	
+	// h(n) value for manhattan distance heuristic
 	private int getManhattanDistance()
 	{
 		int ans=0;
 		int[] index= new int[2];
 		
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < rows; i++)
 		{
-			for (int j = 0; j < 3; j++)
+			for (int j = 0; j < columns; j++)
 			{
 				if(this.positions[i][j] == 0)
 				{
@@ -87,13 +94,14 @@ public class StateNode implements Comparable<StateNode> {
 		return ans;
 	}
 	
+	// Function to get current number's position in goal state
 	private int[] findPositionInGoal(int value)
 	{
 		int[] ans = new int[2];
 		
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < rows; i++)
 		{
-			for (int j = 0; j < 3; j++)
+			for (int j = 0; j < columns; j++)
 			{
 				if(Project.goal[i][j] == 0)
 				{
@@ -111,22 +119,24 @@ public class StateNode implements Comparable<StateNode> {
 		return ans;
 	}
 	
+	// This function gives all states possible after one move
 	public ArrayList<StateNode> getChildrens()
 	{
 		ArrayList<StateNode> childrens = new ArrayList<StateNode>();
 		
-		for(int i = 0; i < 3; i++)
+		for(int i = 0; i < rows; i++)
 		{
-			for(int j = 0; j < 3; j++)
+			for(int j = 0; j < columns; j++)
 			{
 				if(this.positions[i][j] == 0)
 				{
+					//Top Move
 					if(i-1>=0)
 					{
-						int[][] child = new int[3][3];
-						for (int x = 0; x < 3; x++)
+						int[][] child = new int[rows][columns];
+						for (int x = 0; x < rows; x++)
 						{
-							for(int y = 0; y < 3; y++)
+							for(int y = 0; y < columns; y++)
 							{
 								child[x][y] = this.positions[x][y];
 							}
@@ -135,12 +145,14 @@ public class StateNode implements Comparable<StateNode> {
 						 StateNode b = new StateNode(child,this.gn+1, StateNode.input_type);
 						 childrens.add(b);
 					}
+					
+					//Left Move
 					if(j-1>=0)
 					{
-						int[][] child = new int[3][3];
-						for (int x = 0; x < 3; x++)
+						int[][] child = new int[rows][columns];
+						for (int x = 0; x < rows; x++)
 						{
-							for(int y = 0; y < 3; y++)
+							for(int y = 0; y < columns; y++)
 							{
 								child[x][y] = this.positions[x][y];
 							}
@@ -149,12 +161,14 @@ public class StateNode implements Comparable<StateNode> {
 						 StateNode b = new StateNode(child,this.gn+1, StateNode.input_type);
 						 childrens.add(b);
 					}
-					if(i+1<3)
+					
+					//Bottom move
+					if(i+1<rows)
 					{
-						int[][] child = new int[3][3];
-						for (int x = 0; x < 3; x++)
+						int[][] child = new int[rows][columns];
+						for (int x = 0; x < rows; x++)
 						{
-							for(int y = 0; y < 3; y++)
+							for(int y = 0; y < columns; y++)
 							{
 								child[x][y] = this.positions[x][y];
 							}
@@ -163,12 +177,14 @@ public class StateNode implements Comparable<StateNode> {
 						 StateNode b = new StateNode(child,this.gn+1, StateNode.input_type);
 						 childrens.add(b);
 					}
-					if(j+1<3)
+					
+					//Right move
+					if(j+1<columns)
 					{
-						int[][] child = new int[3][3];
-						for (int x = 0; x < 3; x++)
+						int[][] child = new int[rows][columns];
+						for (int x = 0; x < rows; x++)
 						{
-							for(int y = 0; y < 3; y++)
+							for(int y = 0; y < columns; y++)
 							{
 								child[x][y] = this.positions[x][y];
 							}
@@ -184,6 +200,7 @@ public class StateNode implements Comparable<StateNode> {
 		return childrens;
 	}
 	
+	// Swap function to facilitate move
 	private int[][] swap(int[][] a,int row1, int col1, int row2, int col2)
 	{
 	     int[][] copy = a;
@@ -194,6 +211,8 @@ public class StateNode implements Comparable<StateNode> {
 	     return copy;
 	}
 
+	// This comparator is very important
+	// We want to compare StateNode on function value of that state
 	@Override
 	public int compareTo(StateNode o) {
 		if(this.function_value == o.function_value)
